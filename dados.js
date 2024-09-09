5881c445f4202d22cafcd78ea71aadb0fe7362e2
Renato Calisto <99683851+RenatoCCS@users.noreply.github.com>
2024-09-09 14:36:53 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,25 @@
+let dados = [
+  {
+    titulo: 'Rayssa Leal',
+    descricao:
+      'Rayssa Leal é uma skatista brasileira, conhecida como "Fadinha", que conquistou o mundo com seu talento, manobras e carisma.',
+    link: 'https://pt.wikipedia.org/wiki/Rayssa_Leal',
+    tags: 'skate  fada sk8',
+  },
+
+  {
+    titulo: 'Beatriz Souza',
+    descricao:
+      'Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro na categoria +78kg.',
+    link: 'https://pt.wikipedia.org/wiki/Beatriz_Souza',
+    tags: 'judô judo judoka',
+  },
+
+  {
+    titulo: 'Rebeca Andrade',
+    descricao:
+      'Rebeca Andrade é uma ginasta artística brasileira que se destacou nos Jogos Olímpicos de Tóquio 2020 e Paris 2024, conquistando diversas medalhas.',
+    link: 'https://pt.wikipedia.org/wiki/Rebeca_Andrade',
+    tags: 'ginástica ginastica ouro',
+  },
+];
